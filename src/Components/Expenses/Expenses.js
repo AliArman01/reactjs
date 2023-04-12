ab8278696b97a0397d478b7044dfccaa978ccfb1
AliArman01 <arman.ali@technomark.io>
2023-04-12 19:08:58 +0530

    first commit

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import ExpenseItem from "./ExpenseItem";
+
 
 import Card from "../UI/Card";
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 
@@ -11,24 +12,20 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
- 
-  const filteredExpenses = props.items.filter(expense=>{
+
+  const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
   return (
     <div>
-    <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {filteredExpenses.map((expense) =>(
-      <ExpenseItem 
-      key = {expense.id}
-      title = {expense.title} 
-      amount = {expense.amount} 
-      date = {expense.date}
-      />
-      ))}
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items = {filteredExpenses}/>
+      </Card>
     </div>
   );
 }
